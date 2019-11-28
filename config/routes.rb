bca4fcd4559b82d9b6e84241c1fb61bb49bb5cc3
saintaze <ayez_ahmed@hotmail.com>
2019-11-28 21:19:50 +0500

    finish basic user

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,15 @@
 Rails.application.routes.draw do
-
   root                           "static_pages#home"
   
   get       '/contact',      to: 'static_pages#contact'
   get       '/about',        to: 'static_pages#about'
   
-  # get       '/signup',       to: 'users#new'
+  get       '/signup',       to: 'users#new'
   # get       '/login',        to: 'sessions#new'
   # post      '/login',        to: 'sessions#create' 
   # delete    '/logout',       to: 'sessions#destroy' 
 
-  # resources :users,          only: [:create]   
+  resources :users,          only: [:create, :show]   
   # resources :posts,          only: [:index, :new, :create, :destroy] 
 
   # CATCH ALL, the name unmatched_route is just random name * suppodesly captures all the domanin and then any thing after that and this must be last route in file
