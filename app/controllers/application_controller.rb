818e4a88b2ca86ff3a6f1b0d870350b7e593d8c1
saintaze <ayez_ahmed@hotmail.com>
2019-12-04 05:00:42 +0500

    make prod ready and add error pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,40 @@
 class ApplicationController < ActionController::Base
+  # after_action :return_errors, only: [:page_not_found, :server_error]
+  
   include SessionsHelper
+
+  # when authenticity error is thrown which is when post request is made without or with invalid CSRF token catch it with this action
+  rescue_from ActionController::InvalidAuthenticityToken, with: :rescue_422
+
+  def rescue_422
+    flash[:danger] = "Invalid submission"
+    redirect_to root_url
+  end
+
+
+  def page_not_found
+      @status = 404
+      @layout = "application"
+      @template = "not_found_error"
+      render 'errors/not_found_error'
+
+  end
+
+  def server_error
+      @status = 500
+      @layout = "error"
+      @template = "internal_server_error"
+  end
+
+  private
+
+  def return_errors
+      respond_to do |format|
+            format.html { render template: 'errors/' + @template, layout: 'layouts/' + @layout, status: @status }
+            format.all  { render nothing: true, status: @status }
+      end
+  end
+
+
+  
 end
